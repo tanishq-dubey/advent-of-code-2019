09a88477a45c1f38739565bd0bdf2b425631c548
Tanishq Dubey <tdubey@clearstreet.io>
2019-12-02 15:59:11 -0500

    day 1 solution with pt 2

--- d1/main.cpp
+++ d1/main.cpp
@@ -4,18 +4,36 @@
 
 using namespace std;
 
-int main() {
-  string mass_file;
-  cout << "Enter file: ";
-  cin >> mass_file;
+int fuel_calc(int mass) {
+  return floor(mass/3.0) - 2;
+}
 
-  ifstream infile(mass_file);
+int fuel_calc_repeated(int mass) {
+  if (mass <= 0) {
+    return 0;
+  }
+
+  int tmp = fuel_calc(mass);
+  if (tmp <= 0) {
+    return 0;
+  }
+  int tmp2 = fuel_calc_repeated(tmp);
+  return tmp + tmp2;
+}
+
+int main() {
+  ifstream infile("input.txt");
 
   int mass;
   int total = 0;
+  int total_rep = 0;
+
   while(infile >> mass) {
-    total = total + floor(mass/3.0) - 2;
+    total = total + fuel_calc(mass);
+    total_rep = total_rep + fuel_calc_repeated(mass);
   }
 
   cout << "Fuel needed: " << total << endl;
+  cout << "Fuel needed accounting for fuel: " << total_rep << endl;
+  cout << "For fun, the difference: " << total_rep - total << endl;
 }
