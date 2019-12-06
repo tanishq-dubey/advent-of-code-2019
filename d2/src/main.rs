62ad3fc9a32a474182fd4723e0b8a3ca9b189c40
Tanishq Dubey <tanishq.dubey@gmail.com>
2019-12-06 02:40:14 -0500

    day 2 done

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -8,10 +8,8 @@ fn mul(dat: &mut [i32], pc: &usize) {
     dat[dat[*pc + 3] as usize] = dat[dat[*pc + 1] as usize] * dat[dat[*pc + 2] as usize];
 }
 
-fn main() {
-    let mut prog = fs::read_to_string("./input").expect("Had an error reading file");
-    prog.pop();
-    let prog_split = prog.split(",");
+fn run(dstr: &str, noun: i32, verb: i32) -> i32 {
+    let prog_split = dstr.split(",");
 
     let datstr = prog_split.collect::<Vec<&str>>();
     let mut dat = Vec::new();
@@ -21,15 +19,13 @@ fn main() {
     }
 
     // Pre run fixes
-    //dat[1] = 12;
-    //dat[2] = 2;
+    dat[1] = noun;
+    dat[2] = verb;
 
-    println!("{:?}", dat);
     let mut pc : usize = 0;
     while pc < dat.len() {
         match dat[pc] {
             1 => {
-                println!("{} Doing ADD", pc);
                 if pc + 3 > dat.len() {
                     panic!("On ADD opcode, but not enough data left to add");
                 }
@@ -37,7 +33,6 @@ fn main() {
                 pc = pc + 4;
             }
             2 => { 
-                println!("{} Doing MUL", pc);
                 if pc + 3 > dat.len() {
                     panic!("On MUL opcode, but not enough data left to add");
                 }
@@ -52,7 +47,21 @@ fn main() {
             }
         }
     }
+    return dat[0];
+}
 
-    println!("{:?}", dat);
-    println!("Zero Position value is: {}", dat[0])
+fn main() {
+    let mut prog = fs::read_to_string("./input").expect("Had an error reading file");
+    prog.pop();
+    let val = run(&prog, 12, 2);
+    println!("Zero Position value is: {}", val);
+    for x in 0..99 {
+        for y in 0..99 {
+            let tmp = run(&prog, x, y);
+            if tmp == 19690720 {
+                println!("Zero Position value is: {}", tmp);
+                println!("100 * noun {} + verb {}: {}", x, y, 100 * x + y);
+            }
+        }
+    }
 }
