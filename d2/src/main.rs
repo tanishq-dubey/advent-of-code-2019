46cf21fb4253ff87c70c779f1cb3565d89449f99
Tanishq Dubey <tanishq.dubey@gmail.com>
2019-12-06 02:26:39 -0500

    and again

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
 
 fn add(dat: &mut [i32], pc: &usize) {
-    dat[*pc + 3] = dat[*pc + 1] + dat[*pc + 2];
+    dat[dat[*pc + 3] as usize] = dat[dat[*pc + 1] as usize] + dat[dat[*pc + 2] as usize];
 }
 
 fn mul(dat: &mut [i32], pc: &usize) {
-    dat[*pc + 3] = dat[*pc + 1] * dat[*pc + 2];
+    dat[dat[*pc + 3] as usize] = dat[dat[*pc + 1] as usize] * dat[dat[*pc + 2] as usize];
 }
 
 fn main() {
@@ -20,20 +20,39 @@ fn main() {
         dat.push(int);
     }
 
+    // Pre run fixes
+    //dat[1] = 12;
+    //dat[2] = 2;
+
+    println!("{:?}", dat);
     let mut pc : usize = 0;
     while pc < dat.len() {
         match dat[pc] {
-            1 => { 
+            1 => {
+                println!("{} Doing ADD", pc);
+                if pc + 3 > dat.len() {
+                    panic!("On ADD opcode, but not enough data left to add");
+                }
                 add(dat.as_mut_slice(), &pc); 
                 pc = pc + 4;
             }
             2 => { 
+                println!("{} Doing MUL", pc);
+                if pc + 3 > dat.len() {
+                    panic!("On MUL opcode, but not enough data left to add");
+                }
                 mul(dat.as_mut_slice(), &pc); 
                 pc = pc + 4;
+            }
+            99 => {
+                pc = dat.len() + 1;
             }
             _ => {
                panic!("Unknown opcode {:#?}", dat[pc]) 
             }
         }
     }
+
+    println!("{:?}", dat);
+    println!("Zero Position value is: {}", dat[0])
 }
